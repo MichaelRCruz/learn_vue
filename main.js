ae83dd276263375c54ce6fa6ed46847141284bd1
Michael Cruz <michael.cruz@uwalumni.com>
2017-01-28 23:17:00 -0800

    components in components

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 Vue.component('task-list', {
 
-  template: '<li><slot></slot></li>',
+  template:
+    `<div>
+        <task v-for="task in tasks">{{ task.description }}</task>
+    </div>
+    `,
+
   data() {
     return {
-      message: 'sup'
+      tasks: [
+        { description: 'write code', completed: false },
+        { description: 'rewrite resume', completed: false },
+        { description: 'call mom', completed: false },
+        { description: 'return clothes to store', completed: false },
+        { description: 'be awesome', completed: false },
+        { description: 'go running', completed: false },
+        { description: 'profit', completed: false }
+      ]
     }
   }
 });
+
+Vue.component('task', {
+
+  template: '<li><slot></slot></li>',
+
+});
+
+
 
 new Vue({
   el: '#root'
